7d6ca48ef831168cc6389a4bac29ff5229ceba73
FernandoRubiales <fernandorubiales7@gmail.com>
2025-12-19 19:27:02 -0300

    Output serializers para creacion de un gasto

--- gastosCompartidos/gestion/seriallizers.py
+++ gastosCompartidos/gestion/seriallizers.py
@@ -3,7 +3,8 @@ from gastosCompartidos.gestion.models import Gasto, Participante, Transaccion, U
 from decimal import Decimal
 from django.core.exceptions import ValidationError
 
-class ParticipanteDatosSerializer(serializers.Serializer):
+#Entrada de datos para la creacion de un gasto
+class ParticipanteSerializer(serializers.Serializer):
     nombre = serializers.CharField(max_length=150)
     email = serializers.CharField(required=False, allow_blank=True)
     usuario_id = serializers.IntegerField(required=False, allow_null = True)
@@ -15,11 +16,11 @@ class ParticipanteDatosSerializer(serializers.Serializer):
         return montoPagado
 
 
-class CrearGastoSerializer(serializers.Serializer):
+class GastoSerializer(serializers.Serializer):
     titulo = serializers.CharField(max_length=150)
     montoTotalGasto = serializers.DecimalField(max_digits = 10, decimal_places = 2)
     division_tipo = serializers.ChoiceField(choices=['equitativa', 'personalizada'], default = 'equitativa')
-    participantes = ParticipanteDatosSerializer(many=True)
+    participantes = ParticipanteSerializer(many=True)
 
     #Debemos validar que el titulo no este vacio, validate lo ejecuta automatica Django
     def validate_titulo(self, titulo):
@@ -44,4 +45,86 @@ class CrearGastoSerializer(serializers.Serializer):
             raise ValidationError("El gasto debe tener al menos 2 participantes")
         return participantes
 
-#Devolucion de los datos del gasto creado
+#Respuesta de datos para la visualizacion de un gasto
+class ParticipanteResponseSerializer(serializers.ModelSerializer):
+    
+    monto_pendiente = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
+    debe_dinero = serializers.BooleanField(read_only=True)
+    le_deben_dinero = serializers.BooleanField(read_only=True)
+    quedo_saldado = serializers.BooleanField(read_only=True)
+    
+    class Meta:
+        model = Participante
+        fields = [
+            'id',                     
+            'nombreParticipante',     
+            'email_partipante',        
+            'montoPagado',             
+            'monto_a_pagar',          
+            'monto_pendiente',        
+            'debe_dinero',             
+            'le_deben_dinero',        
+            'quedo_saldado',          
+            'estado_pago'              
+        ]
+        read_only_fields = [
+            'id',
+            'nombreParticipante',
+            'email_partipante',
+            'montoPagado',
+            'monto_a_pagar',
+            'monto_pendiente',
+            'debe_dinero',
+            'le_deben_dinero',
+            'quedo_saldado',          
+            'estado_pago'              
+        ]
+
+
+class GastoResponseSerializer(serializers.ModelSerializer):
+    
+    # URL para compartir
+    url_compartible = serializers.SerializerMethodField()
+    
+    # Nombre del usuario creador 
+    creador_nombre = serializers.CharField(source='creador.username', read_only=True)
+    
+    # Anida los participantes con toda su información
+    participantes = ParticipanteResponseSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Gasto
+        fields = [
+            'id',                     
+            'titulo',                 
+            'montoTotalGasto',        
+            'division_tipo',           
+            'estado',                  
+            'link_gasto',              
+            'url_compartible',         
+            'creador_nombre',         
+            'fechaCreacion',           
+            'participantes'            
+        ]
+        read_only_fields = [
+            'id',
+            'titulo',
+            'montoTotalGasto',
+            'division_tipo',
+            'estado',
+            'link_gasto',
+            'url_compartible',
+            'creador_nombre',
+            'fechaCreacion',
+            'participantes'
+        ]
+    
+   # def get_url_compartible(self, obj):
+       
+       # from django.conf import settings
+        
+        # Obtener URL del frontend desde settings
+       # base_url = getattr(settings, 'FRONTEND_URL', 'http://localhost:3000')
+        
+        # Retornar URL completa
+       # return f"{base_url}/gasto/{obj.link_gasto}"
